180d514a311d698aff4eb9537713e865ea0418fd
Kwon-Genie <kwongenie26@gmail.com>
2020-12-26 18:59:01 +0900

    [ASP.NET&Core 를 다루는 기술] 3장 ASP.NET 웹 폼  - 서버 컨트롤 개체 및 이벤트 처리기 생성  - ASP.NET 출력 명령어

---
+++ DevASPNET/DevASPNET/FrmASPNET.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DevASPNET
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Web 화면에 출력해주는 ASP.NET 출력 명령어
+            Response.Write("반갑습니다<br/>");
+
+        }
+    }
+}

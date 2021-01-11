f74641427bca7792780da634085ef95091b8911c
Kwon-Genie <kwongenie26@gmail.com>
2021-01-11 20:37:25 +0900

    6 - CheckBoxList Control

---
+++ DevASPNET/DevASPNET/Chapter6/FrmCheckBoxList.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DevASPNET.Chapter6
+{
+    public partial class FrmCheckBoxList : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnOK_Click(object sender, EventArgs e)
+        {
+            string strMsg = String.Empty;
+
+            for (int i=0; i < lstHobby.Items.Count; i++)
+            {
+                if (lstHobby.Items[i].Selected)
+                {
+                    strMsg += lstHobby.Items[i].Text + "<br />";
+                }
+            }
+            lblDisplay.Text = strMsg;
+
+        }
+    }
+}

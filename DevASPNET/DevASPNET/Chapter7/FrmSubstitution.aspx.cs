2bac9238337b291921a953dee3f058c22dee495c
Kwon-Genie <kwongenie26@gmail.com>
2021-01-18 22:01:22 +0900

    7 - Substitution Control

---
+++ DevASPNET/DevASPNET/Chapter7/FrmSubstitution.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DevASPNET.Chapter7
+{
+    public partial class FrmSubstitution : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblCachedLabel.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        public static string GetCurrentTime(HttpContext context)
+        {
+            return DateTime.Now.ToLongTimeString();
+        }
+    }
+}
